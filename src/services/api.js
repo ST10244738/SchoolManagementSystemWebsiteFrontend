d41fffe11fd816a892cb432f4fb6cbe54382d761
Ontlametse <130988489+ST10244738@users.noreply.github.com>
2025-11-12 11:49:21 +0200

    Updates

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8081/api';
+const API_BASE_URL = 'https://schoolmanagementsystemwebsite-1.onrender.com/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
